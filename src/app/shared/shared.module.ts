7d8d130b9f00c84e003840fc2a86f4c3e15e8832
Tatiana Gutiérrez <90115998+tatianagutierrez@users.noreply.github.com>
2024-03-21 12:02:52 -0300

    implemento enrutamiento sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
@@ -10,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
