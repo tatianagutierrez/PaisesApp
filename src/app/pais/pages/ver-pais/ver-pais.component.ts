8558219f5cd9584d652a07017248f494e0dc01d3
Tatiana Gutiérrez <90115998+tatianagutierrez@users.noreply.github.com>
2024-03-22 20:09:52 -0300

    agrego lógica de página ver información de país

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../intefaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styles: ``
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit{
+
+  pais!: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private paisService: PaisService) {}
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(param => this.paisService.getPaisPorCodigo(param['id'])),
+        tap(console.log)
+      )
+      .subscribe(pais => this.pais = pais);
+  }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Country } from '../intefaces/pais.interface';
 
 @Injectable({
@@ -21,5 +21,14 @@ export class PaisService {
     const url: string = `${this.apiUrl}/capital/${termino}`;
 
     return this.http.get<Country[]>(url);
+  }
+
+  getPaisPorCodigo(id: string): Observable<Country> {
+    const url: string = `${this.apiUrl}/alpha/${id}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map(paises => paises[0])
+      );
   }
 }
